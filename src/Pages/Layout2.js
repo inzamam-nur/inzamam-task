44b9a3c9125ae7bb6d04f98f27136f6a2ba65921
inzamam nur <inzamamnur.14@gmail.com>
2022-12-23 20:27:08 +0600

    date decorate using moment

--- src/Pages/Layout2.js
+++ src/Pages/Layout2.js
@@ -1,38 +1,44 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import moment from 'moment'
+
 
 const Layout2 = () => {
-  const user_info = useLoaderData();
-  return (
-    <div>
-      <h1 className="text-center text-3xl my-3 "> </h1>
-
-      <div className="overflow-x-auto mx-auto max-w-[80%]">
-        <table className="table w-full">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Fullname</th>
-              <th>Date Of birth</th>
-              <th>Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            {user_info?.map((user, index) => {
-              return (
-                <tr>
-                  <th>{index + 1}</th>
-                  <td>{user.fullname}</td>
-                  <td>{user.Date_of_birth}</td>
-                  <td>{user.email}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+    const user_info=useLoaderData();
+    return (
+        <div>
+        <h1 className="text-center text-3xl my-3 ">
+          {" "}
+         
+        </h1>
+        
+        <div className="overflow-x-auto mx-auto max-w-[80%]">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Fullname</th>
+                <th>Date Of birth</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              {user_info?.map((user, index) => {
+                return (
+                  <tr>
+                    <th>{index + 1}</th>
+                    <td>{user.fullname}</td>
+                    <td>{moment(user.Date_of_birth).format("DD-MM-YYYY")}
+</td>
+                    <td>{user.email}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Layout2;
